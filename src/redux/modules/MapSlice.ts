a4f2e5d2576ebfa861338795eee018de4a91b5a6
moojecho <wpfh188@naver.com>
2023-03-21 17:21:16 +0900

    Initial Setup
    Initial Setup

---
+++ src/redux/modules/MapSlice.ts
@@ -0,0 +1,49 @@
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export type map = {
+  mapList: 
+    {
+      _id?:string;
+      key: number;
+      mapNum: string;
+      mapLocation1: number;
+      mapLocation2: number;
+      mapLocationName: string;
+    }[]
+};
+const initialState: map = {
+  mapList: [],
+};
+
+export const __sendMapInfo = createAsyncThunk(
+  "MAPINFO",
+  async (payload: any, thunkAPI) => {
+    console.log(payload);
+    const { data } = await axios.post(`http://localhost:3001/catMap`, payload);
+    console.log(data);
+    return thunkAPI.fulfillWithValue(data);
+  }
+);
+
+export const __receiveMapInfo = createAsyncThunk(
+  "GETMAPINFO",
+  async (payload, thunkAPI) => {
+    const { data } = await axios.get(`http://localhost:3001/catMap`);
+    return thunkAPI.fulfillWithValue(data);
+  }
+);
+
+export const MapSlice = createSlice({
+  name: "catLoctionMap",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(__receiveMapInfo.fulfilled, (state, action) => {
+      console.log(action.payload)
+      state.mapList = action.payload;
+    });
+  }
+});
+export const {} = MapSlice.actions;
+export default MapSlice.reducer;
